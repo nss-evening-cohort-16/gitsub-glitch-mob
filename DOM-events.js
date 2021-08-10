f8217bf6c1f2e9e4dffb8a56aec9d574faf4e2ab
Messi_G <negash4@gmail.com>
2021-08-09 21:25:25 -0500

    header function

--- DOM-events.js
+++ DOM-events.js
@@ -1,5 +1,5 @@
 import { currentUser, injectSampleData } from "./data-functions.js";
-import { pageLayout, bioPanel, footer } from "./DOM-elements.js";
+import { pageLayout, bioPanel, footer, header } from "./DOM-elements.js";
 
 
 export const init = () => {
@@ -7,5 +7,7 @@ export const init = () => {
 
     pageLayout();
     bioPanel(currentUser);
+    header();
     footer();
+ 
 };
