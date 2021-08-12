ac556c47de8d99d83de43cffe60b92ad83db3bec
Craig Wellspring <wellspring.craig@gmail.com>
2021-08-11 20:44:13 -0500

    add data

--- DOM-functions.js
+++ DOM-functions.js
@@ -7,6 +7,8 @@ export const renderContent = () => {
         case "/repos.html":
             listCards(currentUser.repoData, renderRepoCard);
             repoForm();
+
+            currentUser.addRepoData()
             break;
 
         case "/projects.html":
