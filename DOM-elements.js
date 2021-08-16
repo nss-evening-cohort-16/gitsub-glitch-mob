6bd0449af3f1f8d85fb189a4be4b226b362c6a2e
Craig Wellspring <wellspring.craig@gmail.com>
2021-08-16 17:09:31 -0500

    tiny navbar tweak

--- DOM-elements.js
+++ DOM-elements.js
@@ -57,16 +57,16 @@ export const bioPanel = (_currentUser) => {
 };
 
 // Header / NavBar
-export const header = `<nav class="navbar navbar-expand-lg navbar-dark">
+export const header = `
+      <nav class="navbar navbar-expand-lg navbar-dark">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="index.html" id ="Overview">Overview</a>
+                <a class="nav-link" href="index.html" id ="Overview">Overview</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="repos.html" id="Repositories">Repositories</a>
